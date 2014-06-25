e158569e003058e23486218856a1b49b217c8828
Andrew Hayward <github@andrewhayward.net>
2014-06-25 21:40:23 +0100

    Updating static bits and pieces

--- badgus/base/static/js/base.js
+++ badgus/base/static/js/base.js
@@ -3,6 +3,7 @@
  */
 
 // Set up Google Analytics
+/*
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-32268013-1']);
 _gaq.push(['_setDomainName', 'badg.us']);
@@ -12,6 +13,7 @@ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+*/
 
 $(document).ready(function () {
 
